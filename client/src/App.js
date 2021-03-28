940f75f6515d3761f406e0315b3f525a713c7cad
AnnMarkham <ann.e.markham@outlook.com>
2021-03-28 10:35:21 -0700

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from '@apollo/client';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider, createHttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Auth from './utils/auth';
 
 import Home from "./pages/Home";
 import Nav from './components/Nav';
@@ -13,17 +14,25 @@ import SingleStory from './pages/SingleStory';
 import NoteForm from './components/NoteForm';
 
 
-const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem('id_token');
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    });
-  },
-  uri: '/graphql'
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = Auth.getToken();
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
-const path = require('path');
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+// serve up front-end code in production
+const path = require('path');
 
-const { authMiddleware } = require('./utils/auth');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
-//context authMiddleware assures that every requrest performs authentication check and the updated request object is passed to resolvers as context. 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
-//from here to db.once = sets up ability of back end server's code to serv up React front-end code
-// requires path and code in root package.json if-env
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
@@ -30,9 +28,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 });
 
 db.once('open', () => {
