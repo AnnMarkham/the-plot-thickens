d1c2f1e9d06e7a3c470eee3b7b2d928444f764e9
AnnMarkham <ann.e.markham@outlook.com>
2021-03-28 11:33:54 -0700

    heroku another attempt

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/client';
 import ApolloClient from 'apollo-boost';
 
--- server/server.js
+++ server/server.js
@@ -1,36 +1,46 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const path = require('path');
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 
-const db = require("./config/connection");
+const { authMiddleware } = require('./utils/auth');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+// create a new Apollo server and pass in our schema data
+//context authMiddleware assures that every requrest performs authentication check and the updated request object is passed to resolvers as context. 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: authMiddleware
 });
 
+//from here to db.once = sets up ability of back end server's code to serv up React front-end code
+// requires path and code in root package.json if-env
+// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(` API Server running on Port ${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
